7d298201591050a730ba8e4e63e22f6e8005483e
IbnuParvez <parvezfatma98@gmail.com>
2025-12-27 17:09:01 +0300

    efficient

---
+++ example.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// Attach the function to the prototype instead
+Book.prototype.info = function() {
+  const readStatus = this.read ? "read" : "not read yet";
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+};
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+console.log(theHobbit.info()); // Works exactly the same!
